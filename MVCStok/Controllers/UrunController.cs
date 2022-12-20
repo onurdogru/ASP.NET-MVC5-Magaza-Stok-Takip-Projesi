1a98e9e7374e4f41681e84668eb1d685e1099f08
Onur <onurdogruiletisim@gmail.com>
2022-12-20 23:02:28 +0300

    dropdl

--- MVCStok/Controllers/UrunController.cs
+++ MVCStok/Controllers/UrunController.cs
@@ -20,5 +20,27 @@ namespace MVCStok.Controllers
             var urunler = db.tblurunler.ToList();
             return View(urunler);
         }
+
+        //14.ADIM : Yeni Ürün Ekleme
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            //15.ADIM başlangıç //ktg ismindeki değerime aşağıdaki değerleri atamış oldul.
+            List <SelectListItem> ktg=(from x in db.tblkategori.ToList()
+                                       select new SelectListItem
+                                       {
+                                           Text=x.ad,
+                                           Value=x.id.ToString()
+                                       }).ToList();
+            ViewBag.drop = ktg; //drop isminde bir değer oluştururuz, ktg'deki değerleri tutmakla görevlidir.
+                            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniUrun(tblurunler t)
+        {
+            return View();
+        }
     }
 }
